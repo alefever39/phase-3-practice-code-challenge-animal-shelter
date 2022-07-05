f0c46d05761865df9a348c0b16db8c17c62ddad8
Allie LeFever <alefever39@gmail.com>
2022-07-05 12:46:51 -0600

    completed lab

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -1,3 +1,30 @@
 class Adopter < ActiveRecord::Base
+  has_many :pets
+  has_many :shelters, through: :pets
 
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def adopt(pet)
+    if (!pet.adopted?)
+      pet.update(adopted?: true)
+      self.pets << pet
+    end
+  end
+
+  def fav_pet
+    if (self.pets.count > 0)
+      self
+        .pets
+        .group_by { |pet| pet.species }
+        .max_by { |species, value| value.count }[
+        0
+      ]
+    end
+  end
+
+  def self.most_pets
+    self.all.group_by { |adopter| adopter.pets.count }.max[1]
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,16 @@
 class Pet < ActiveRecord::Base
+  belongs_to :adopter
+  belongs_to :shelter
 
+  def self.oldest
+    self.all.max_by { |pet| pet.age }
+  end
+
+  def self.average_age
+    self.all.sum { |pet| pet.age } / self.all.count
+  end
+
+  def in_dog_years
+    self.age * 5
+  end
 end
